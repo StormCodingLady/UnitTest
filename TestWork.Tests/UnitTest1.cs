07b70133be3e683bba85c829c1a35bb6ae99d332
stormiarias <sanduagaarias@gmail.com>
2016-07-08 10:09:23 -0700

    initial commit

---
+++ TestWork.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestWork.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool result = Program.CheckSign();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ TestWork/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWork
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckSign();
+            Console.ReadLine();
+        }
+
+        public static bool CheckSign()
+        {
+            int a = 2;
+            int b = -4;
+            int c = -6;
+            bool sign = true;
+
+            if (a == 0 || b == 0 || c == 0) // 0 or 0 or 0
+            {
+                Console.WriteLine("Does not compute to negative or positive.");
+            }
+            else if (a > 0 && b > 0 && c > 0) // + + +
+            {
+                Console.WriteLine("The product is '+'.");
+            }
+            else if (a < 0 && b < 0 && c < 0) // - - -
+            {
+                Console.WriteLine("The product is '-'.");
+                sign = false;
+            }
+            else if (a > 0 && b < 0 && c < 0) // + - -
+            {
+                Console.WriteLine("The product is '+'.");
+            }
+            else if (a < 0 && b < 0 && c > 0) // - - +
+            {
+                Console.WriteLine("The product is '+'.");
+            }
+            else if (a > 0 && b > 0 && c < 0) // + + -
+            {
+                Console.WriteLine("The product is '-'.");
+                sign = false;
+            }
+            else if (a < 0 && b > 0 && c > 0) // - + +
+            {
+                Console.WriteLine("The product is '-'.");
+                sign = false;
+            }
+            else
+            {
+                Console.WriteLine("idk");
+            }
+
+            return sign;
+        }
+    }
+}
