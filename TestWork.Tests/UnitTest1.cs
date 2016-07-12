0cf8dda935adfa3c83e72bf22b1e7c42c31d3be2
stormiarias <sanduagaarias@gmail.com>
2016-07-12 15:17:23 -0700

    It works; numerically.

--- TestWork.Tests/UnitTest1.cs
+++ TestWork.Tests/UnitTest1.cs
@@ -9,19 +9,25 @@ namespace TestWork.Tests
         [TestMethod]
         public void ProductAnalysis_CheckOutcomes_LargestOfThree()
         {
-            int[,] diagnose;
-
-            diagnose  {
+            int [,] diagnose = {
                 { 2, 4, 6 },
                 { 6, 4, 2 },
-                { 4, 6, 2 },
+                { 4, 6, 2 }
+            };
+
+            int numberRows = diagnose.GetLength(0);
+            int numberColumns = diagnose.GetLength(1);
+
+            int resultBest = 6;
+
+            for (int i = 0; i < numberRows; i++)
+            {
+                int a = diagnose[i, 0];
+                int b = diagnose[i, 1];
+                int c = diagnose[i, 2];
+                int result = Program.CheckValue(a, b, c);
+                Assert.IsTrue(resultBest == result);
             }
-
-
-
-            bool result = Program.CheckSign();
-
-            Assert.IsTrue(result);
         }
     }
 }
--- TestWork/Program.cs
+++ TestWork/Program.cs
@@ -10,52 +10,96 @@ namespace TestWork
     {
         static void Main(string[] args)
         {
-            int a = 6;
-            int b = 4;
-            int c = 2;
-
-            Console.ReadLine();   
-        }
-
-        public static bool CheckValue(int a, int b, int c)
-        {
-            if (a > b && a > c)
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int intInput;
+            bool noError = false;
+            bool noError2 = false;
+            bool noError3 = false;
+            
+            while (noError == false)
             {
-                Console.WriteLine("Integer 'a' is largest.");
-            }
-            else
-            {
-                if (b > c)
+                Console.WriteLine("Integer 'a' equals...");
+                Console.WriteLine("(Enter an integer from the number pad)");
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out intInput);
+                if (isInt)
                 {
-                    Console.WriteLine("Integer 'b' is largest.");
+                    a = intInput;
+                    noError = true;
                 }
                 else
                 {
-                    Console.WriteLine("Integer 'c' is largest.");
+                    Console.WriteLine("Not a valid input.");
+                    Console.WriteLine();
+                }
+
+            }
+
+            while (noError2 == false)
+            {
+                Console.WriteLine("Integer 'b' equals...");
+                Console.WriteLine("(Enter an integer from the number pad)");
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out intInput);
+                if (isInt)
+                {
+                    b = intInput;
+                    noError2 = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input.");
+                    Console.WriteLine();
+                }
+
+            }
+
+            while (noError3 == false)
+            {
+                Console.WriteLine("Integer 'c' equals...");
+                Console.WriteLine("(Enter an integer from the number pad)");
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out intInput);
+                if (isInt)
+                {
+                    c = intInput;
+                    noError3 = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input.");
+                    Console.WriteLine();
+                }
+            }
+
+
+
+            CheckValue(a, b, c);
+            Console.ReadLine();
+        }
+
+        
+
+        public static int CheckValue(int a, int b, int c)
+        {
+            if (a >= b && a >= c)
+            {
+                return a; 
+            }
+            else
+            {
+                if (b >= c)
+                {
+                    return b;
+                }
+                else
+                {
+                    return c;
                 }
             }
         }
     }
-
-    public class SnowMan
-    {
-        static private int sizeOfNose;
-        bool changeMe;
-
-        public string ThrowRock(int a, string bravo)
-        {
-            return "hello";
-        }
-
-        static public int TossStone(string hey, int b)
-        {
-            return 2;
-        }
-
-        public void GoCrazy()
-        {
-            changeMe = !changeMe;
-            return;
-        }
-    }
 }
+
